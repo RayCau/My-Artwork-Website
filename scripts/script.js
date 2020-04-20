10a4427e86a69bde929d88c6ae87ef336414c7a3
RayCau <raycaucci7@hotmail.com>
2020-04-20 16:29:52 +1000

    Index.html features the home page of my website. This includes a stylised navbar, upper left logo, a hello message card and a couple of artwork placeholders. In the images folder, there are generated favicon images and the placeholder images. In the pages are some default html files for the navbar to link to (still unimplemented). There is a scripts folder for javascript files, and a styles folder which contains, currently, the styling for the index.html file.

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+function on() {
+    document.getElementById("stanceoverlaydiv").style.display = "block";
+    document.getElementById("stanceoverlaydiv").style.visibility = "visible";
+  }
+  
+  function off() {
+    document.getElementById("stanceoverlaydiv").style.display = "none";
+  }
